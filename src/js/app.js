c9508a127b05e7a43cc3046f98798375cf1735e8
maria <masha@coderiver.com.ua>
2018-03-01 23:58:48 +0200

    [fix]project fixes

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,16 @@
 import 'slick-carousel';
 
-$('.slider').slick({
+$('.slider-js').slick({
   infinite: true,
   slidesToShow: 1,
   slidesToScroll: 1,
   arrows: true,
-  nextArrow: '<span class="arrow1"><img src="img/page-1.png" alt=""></span>',
-  prevArrow: '<span class="arrow2"><img src="img/page-2.png" alt=""></span>'
+  nextArrow: '<span class="arrow-prev"><img src="img/page-1.png" alt=""></span>',
+  prevArrow: '<span class="arrow-next"><img src="img/page-2.png" alt=""></span>'
 });
 
 
-$('.hover__toggle input').change((e) => {
+$('.radio-js').change((e) => {
   const dom = e.target;
   const parent = dom.closest('.custom');
   parent.classList[dom.classList.contains('black') ? 'add' : 'remove']('black');
