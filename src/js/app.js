796a6bf673e45302258d79376b448e588dbf2236
maria <masha@coderiver.com.ua>
2018-03-07 19:00:51 +0200

    fixes form trello

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,3 @@
-import 'slick-carousel';
-
-$('.slider-js').slick({
-  infinite: true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false
-});
-
 
 $('.radio-js').change((e) => {
   const dom = e.target;
