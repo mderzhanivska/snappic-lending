f9bac6a11abbc7ae751731a83b8a925135ca9b7e
maria <masha@coderiver.com.ua>
2018-03-07 14:13:52 +0200

    [add]fixes

--- src/js/app.js
+++ src/js/app.js
@@ -4,9 +4,7 @@ $('.slider-js').slick({
   infinite: true,
   slidesToShow: 1,
   slidesToScroll: 1,
-  arrows: true,
-  nextArrow: '<span class="arrow-prev"><img src="img/page-1.png" alt=""></span>',
-  prevArrow: '<span class="arrow-next"><img src="img/page-2.png" alt=""></span>'
+  arrows: false
 });
 
 
