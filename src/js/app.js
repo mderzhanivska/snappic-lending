81633613712512238146224f95269cfdf1fc87d0
maria <masha@coderiver.com.ua>
2018-03-14 10:45:04 +0200

    [add]switching buttons

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,36 @@
 
-$('.radio-js').change((e) => {
-  const dom = e.target;
-  const parent = dom.closest('.custom');
-  parent.classList[dom.classList.contains('black') ? 'add' : 'remove']('black');
-});
+(function() {
+
+  $('.radio-js').change((e) => {
+    const dom = e.target;
+    const parent = dom.closest('.custom');
+    parent.classList[dom.classList.contains('black') ? 'add' : 'remove']('black');
+  });
+
+  const layout = document.getElementById('layout');
+  const btnLayout = document.getElementById('lay_control');
+
+  if (btnLayout) {
+    btnLayout.addEventListener('click', callback, false);
+  }
+
+  function callback(e) {
+    if (e.target === e.currentTarget) return;
+
+    const dom = e.target;
+    const activeEl = dom.parentElement.querySelector('.active');
+    !activeEl || activeEl.classList.remove('active');
+
+    dom.classList.add('active');
+    layout.className = dom.dataset.layout;
+  }
+
+})();
+
+
+
+
+
+
 
 
